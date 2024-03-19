fe3009a27a1fbd3cf7b3aa5be463b8b5996da244
dani8377 <duendaniel@live.dk>
2024-03-19 10:22:21 +0100

    copy .mat files

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -2,6 +2,26 @@ import cv2
 import os
 import sys
 import subprocess
+import shutil
+
+def copy_mat_files(source_dir, destination_dir):
+    # Ensure source directory exists
+    if not os.path.exists(source_dir):
+        print(f"Source directory '{source_dir}' does not exist.")
+        return
+    
+    # Ensure destination directory exists, create if not
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+    
+    # Iterate through files in source directory
+    for filename in os.listdir(source_dir):
+        if filename.endswith('.mat'):
+            source_file = os.path.join(source_dir, filename)
+            destination_file = os.path.join(destination_dir, filename)
+            shutil.copy2(source_file, destination_file)
+            print(f"Copied '{source_file}' to '{destination_file}'")
+
 
 def open_image(image_path):
     if sys.platform == "darwin":  # Since you're using a MacBook
@@ -14,6 +34,7 @@ def open_image(image_path):
 # Instead of getting the paths from command line arguments, prompt the user for input
 image_dir = input("Enter the path to the directory with images: ")
 output_dir = input("Enter the path to the directory where blurred images will be saved: ")
+move_mat_files = input("Do you want to move .mat files too (yes or no): ")
 
 # Make sure output directory exists
 if not os.path.exists(output_dir):
@@ -21,6 +42,9 @@ if not os.path.exists(output_dir):
 
 # Get all filenames in the directory
 filenames = os.listdir(image_dir)
+
+if move_mat_files == "yes":
+    copy_mat_files(image_dir, output_dir)
 
 # Loop through all filenames
 for filename in filenames:
@@ -37,7 +61,7 @@ for filename in filenames:
             blurred_img = cv2.GaussianBlur(img, (5, 5), 0)
 
             # Create a new filename for the blurred image (optional)
-            output_filename = f"blurred_{filename}"
+            output_filename = f"{filename}"
 
             # Save the blurred image in the output directory
             output_image_path = os.path.join(output_dir, output_filename)
