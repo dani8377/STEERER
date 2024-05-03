e1f142cc67cb770ccbbad82987957ea35581abc2
John cena <131255317+Ertmartinert@users.noreply.github.com>
2024-05-03 21:17:06 +0200

    komnuuu

--- lsf_train.sh
+++ lsf_train.sh
@@ -3,9 +3,9 @@
 #BSUB -J SHHB_Crowd_Train         # Set the job name
 #BSUB -q 'gpuv100'               # Specify the queue/partition
 #BSUB -gpu "num=4:mode=exclusive_process"  # Request 4 GPUs, exclusive access
-#BSUB -n 4                     # Total number of tasks (GPUs in this case)
+#BSUB -n 16                     # Total number of tasks (GPUs in this case)
 #BSUB -R "span[ptile=4]"       # Number of tasks per node
-#BSUB -M 10GB                  # Memory limit per task
+#BSUB -M 20GB                  # Memory limit per task
 #BSUB -W 24:00                # Wall clock limit (188 hours)
 #BSUB -o %J.out                # Standard output
 #BSUB -e %J.err                # Standard error
