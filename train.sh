ae185637cbb263be0da0f3b294f5cc0c65ed5286
John cena <131255317+Ertmartinert@users.noreply.github.com>
2024-05-04 15:40:18 +0200

    hey

--- train.sh
+++ train.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-#BSUB -J SHHB_Crowd_Train         # Set the job name
-#BSUB -q 'gpuv100'               # Specify the queue/partition
-#BSUB -gpu "num=4:mode=exclusive_process"  # Request 4 GPUs, exclusive access
-#BSUB -n 16                     # Total number of tasks (GPUs in this case)
-#BSUB -R "span[ptile=4]"       # Number of tasks per node
-#BSUB -M 20GB                  # Memory limit per task
-#BSUB -W 24:00                # Wall clock limit (188 hours)
-#BSUB -o %J.out                # Standard output
-#BSUB -e %J.err                # Standard error
+# Set the job name
+#BSUB -J SHHB_Crowd_Train
 
+# Specify the queue/partition
+#BSUB -q gpuv100
+
+# Request 4 GPUs, exclusive access
+#BSUB -gpu "num=4:mode=exclusive_process"
+
+# Total number of tasks (GPUs in this case)
+#BSUB -n 16
+
+# Number of tasks per node
+#BSUB -R "span[ptile=4]"
+
+# Memory limit per task
+#BSUB -M 20GB
+
+# Wall clock limit (24 hours)
+#BSUB -W 24:00
+
+# Standard output
+#BSUB -o %J.out
+
+# Standard error
+#BSUB -e %J.err
+
+# Activate the Conda environment
 source ~/miniconda3/bin/activate STEERER
 
 # Run the Python script
