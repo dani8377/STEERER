a363bc3a02607f3f063b8796d30dde62631cff79
John cena <131255317+Ertmartinert@users.noreply.github.com>
2024-05-06 18:11:36 +0200

    hey

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-#BSUB -J Blurred_QNRF          # Set the job name
-#BSUB -q gpuv100                # Specify the queue/partition
-#BSUB -gpu "num=2:mode=exclusive_process"  # Request 2 GPUs, exclusive access
-#BSUB -n 2                      # Total number of tasks (GPUs in this case)
-#BSUB -R "rusage[mem=5000]"     # Memory requirement per task
-#BSUB -W 24:00                  # Wall clock limit (24 hours)
-#BSUB -o %J.out                 # Standard output
-#BSUB -e %J.err                 # Standard error
+#!/bin/sh
+#BSUB -J Blurred_QNRF         # Set the job name
+#BSUB -q gpuv100               # Specify the queue/partition
+#BSUB -gpu "num=2:mode=exclusive_process"  # Request 4 GPUs, exclusive access
+#BSUB -n 8                     # Total number of tasks (GPUs in this case)
+#BSUB -M 5GB                  # Memory limit per task
+#BSUB -W 24:00                # Wall clock limit (188 hours)
+#BSUB -o %J.out                # Standard output
+#BSUB -e %J.err                # Standard error
 
 # Activate environment (if needed)
 source ~/miniconda3/bin/activate STEERER
 
 # Run the program
-python root/STEERER/train_cc.py --config configs/SHHB_final.py --launcher lsf
+sh root/STEERER/train.sh configs/Blurred_SHHB_final.py 0,1
