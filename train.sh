0107e1e18b31f41e21fc5dc1312e423b17aab68b
John cena <131255317+Ertmartinert@users.noreply.github.com>
2024-05-08 11:46:48 +0200

    hey

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
-"#!/bin/sh
+#!/bin/sh
 #BSUB -J Blurred_QNRF         # Set the job name
 #BSUB -q gpuv100               # Specify the queue/partition
-#BSUB -gpu "num=2:mode=exclusive_process"  # Request 4 GPUs, exclusive access
-#BSUB -n 8                     # Total number of tasks (GPUs in this case)
+#BSUB -gpu "num=4:mode=exclusive_process"  # Request 4 GPUs, exclusive access
+#BSUB -n 4                     # Total number of tasks (GPUs in this case)
 #BSUB -M 5GB                  # Memory limit per task
-#BSUB -W 24:00                # Wall clock limit (188 hours)
+#BSUB -W 24:00                # Wall clock limit (24 hours)
 #BSUB -o %J.out                # Standard output
 #BSUB -e %J.err                # Standard error
 
@@ -12,4 +12,4 @@
 source ~/miniconda3/bin/activate STEERER
 
 # Run the program
-sh train.sh configs/Blurred_SHHB_final.py 0,1,2,3"
+sh train.sh configs/SHHB_final.py 0,1,2,3
