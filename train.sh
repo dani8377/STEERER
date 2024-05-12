fe254c85e029638b98e5e93c90b5cae49fafd752
John cena <131255317+Ertmartinert@users.noreply.github.com>
2024-05-12 12:23:38 +0200

    hey

--- train.sh
+++ train.sh
@@ -1,16 +1,35 @@
-#!/bin/sh
-#BSUB -J Blurred_QNRF                # Set the job name
-#BSUB -q gpuv100                      # Specify the queue/partition
-#BSUB -gpu "num=4:mode=exclusive_process"  # Request 4 GPUs, exclusive access
-#BSUB -n 16                           # Total number of tasks (4 GPUs * 4 cores per GPU = 16 cores)
-#BSUB -R "span[ptile=4]"              # Number of cores per node (4 cores per GPU)
-#BSUB -M 5GB                          # Memory limit per task
-#BSUB -W 24:00                        # Wall clock limit (24 hours)
-#BSUB -o %J.out                       # Standard output
-#BSUB -e %J.err                       # Standard error
+# !/usr/bin/env sh
+# ${GPUS:-4}
+# set -x
 
-# Activate environment (if needed)
-source ~/miniconda3/bin/activate STEERER
 
-# Run the program
-sh train.sh configs/Blurred_SHHB_final.py 0,1,2,3
+CONFIG=$1
+GPUS_ID=${2:-0}    #the default gpu_id is 0 
+PORT=${3:-29000}   #the default gpu_id is 1
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+GPU_NUM=0
+
+for ((i=0; i<${#GPUS_ID}; i++)); do
+    if [[ ${GPUS_ID:i:1} =~ [0-9] ]]; then
+        ((GPU_NUM++))
+    fi
+done
+
+source myenv/bin/activate
+
+echo "export CUDA_VISIBLE_DEVICES=$GPUS_ID"
+export CUDA_VISIBLE_DEVICES=${GPUS_ID:-"0"}
+
+
+# torchrun --nproc_per_node=${GPU_NUM} --master_port ${PORT} tools/train_cc.py --cfg ${CONFIG} 
+
+python -m torch.distributed.launch \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPU_NUM \
+    --master_port=$PORT \
+    tools/train_cc.py --cfg=$CONFIG --launcher="pytorch"
